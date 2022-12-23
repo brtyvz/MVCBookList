09eca3eec0d9d99d3f306ae4db07914221048a3e
brtyvz <beratttyavuzz@gmail.com>
2022-12-23 17:42:01 +0300

    commit

---
+++ BookApplication/Enum/GenderEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookApplication.Enum
+{
+    public enum GenderEnum
+    {
+        male,
+        female
+    }
+}
