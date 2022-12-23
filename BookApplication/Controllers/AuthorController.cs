4a27691460c3317947c81c503ca412974ad6b9d2
brtyvz <beratttyavuzz@gmail.com>
2022-12-23 16:23:20 +0300

    Add project files.

---
+++ BookApplication/Controllers/AuthorController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookApplication.Models.Entitiy;
+
+namespace BookApplication.Controllers
+{
+    public class AuthorController : Controller
+    {
+        // GET: Author
+        MvcDbLibraryEntities db = new MvcDbLibraryEntities();
+        public ActionResult Index(string p)
+        {
+            var values = from d in db.TBLAUTHOR select d;
+            if (!string.IsNullOrEmpty(p))
+            {
+                values = values.Where(m => m.AUTHORNAME.Contains(p));
+            }
+            //empty search
+            return View(values.ToList());
+
+            //var values = db.TBLAUTHOR.ToList();
+            //return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewAuthor() {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult NewAuthor(TBLAUTHOR p1)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("NewAuthor");
+            }
+
+            db.TBLAUTHOR.Add(p1);
+            db.SaveChanges();
+            return View();
+        }
+    }
+}
---
+++ BookApplication/Controllers/BookController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BookApplication.Models.Entitiy;
+
+namespace BookApplication.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        MvcDbLibraryEntities db = new MvcDbLibraryEntities();
+        public ActionResult Index(string p)
+        {
+            var values = from d in db.TBLBOOKS select d;
+            if (!string.IsNullOrEmpty(p)) {
+                values = values.Where(m => m.BOOKNAME.Contains(p));
+            }
+            //empty search
+            return View(values.ToList());
+            
+            //var values = db.TBLBOOKS.ToList();
+            //return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult NewBook()
+        {
+            List<SelectListItem> values = (from i in db.TBLCATEGORIES.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.CATEGORYNAME,
+                                               Value = i.CATEGORYID.ToString()
+                                           }).ToList();
+            ViewBag.value = values;
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult NewBook(TBLBOOKS p1)
+        {
+            var ctg = db.TBLCATEGORIES.Where(m => m.CATEGORYID == p1.TBLCATEGORIES.CATEGORYID).FirstOrDefault();
+            p1.TBLCATEGORIES = ctg;
+            db.TBLBOOKS.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
